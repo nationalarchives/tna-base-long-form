c172a91c62115296a620bead2d16729583bf7a6c
Punal Chotrani <punalsc@gmail.com>
2016-03-09 13:04:25 +0000

    Files for template

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+// Edit as required
+function tnatheme_globals() {
+    global $pre_path;
+    global $pre_crumbs;
+    if (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
+        $pre_path = '';
+        $pre_crumbs = array(
+            'Commercial opportunities' => '/'
+        );
+    } else {
+        $pre_crumbs = array(
+            'About us' => '/about/',
+            'Commercial opportunities' => '/about/commercial-opportunities/'
+        );
+        $pre_path = '/about/commercial-opportunities';
+    }
+}
+// For live environment
+// tnatheme_globals();
+function dequeue_parent_style() {
+    wp_dequeue_style('tna-styles');
+    wp_deregister_style('tna-styles');
+}
+add_action( 'wp_enqueue_scripts', 'dequeue_parent_style', 9999 );
+add_action( 'wp_head', 'dequeue_parent_style', 9999 );
+// Enqueue styles
+function tna_child_styles() {
+    wp_register_style( 'tna-parent-styles', get_template_directory_uri() . '/css/base-sass.css.min', array(), EDD_VERSION, 'all' );
+    //wp_register_style( 'tna-child-styles', get_stylesheet_directory_uri() . '/style.css', array(), '0.1', 'all' );
+    wp_register_style( 'tna-child-styles', get_stylesheet_directory_uri() . '/css/style.css', array(), '0.1', 'all' );
+    wp_enqueue_style( 'tna-parent-styles' );
+    wp_enqueue_style( 'tna-child-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'tna_child_styles' );
