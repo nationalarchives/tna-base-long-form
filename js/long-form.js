dc7cc1c7158ffcc8c7b5ffc0b77edafaa53b75f2
Punal Chotrani <punalsc@gmail.com>
2016-03-17 14:16:25 +0000

    navigation always in active state.

---
+++ js/long-form.js
@@ -0,0 +1,3 @@
+/**
+ * Created by pchotrani on 17/03/16.
+ */
--- tna-long-form.php
+++ tna-long-form.php
@@ -8,38 +8,38 @@ get_header(); ?>
         <ul>
             <li>
                 <a href="#section1" data-number="1">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label arrow_box">Broadly - trenches</span>
+                    <span class="cd-dot active-dot"></span>
+                    <span class="cd-label arrow_box active">Technology and the First World War</span>
                 </a>
             </li>
             <li>
                 <a href="#section2" data-number="2">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label arrow_box">Air warfare</span>
+                    <span class="cd-dot active-dot"></span>
+                    <span class="cd-label arrow_box active">Air warfare</span>
                 </a>
             </li>
             <li>
                 <a href="#section3" data-number="3">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label arrow_box">Ships</span>
+                    <span class="cd-dot active-dot"></span>
+                    <span class="cd-label arrow_box active">Ships</span>
                 </a>
             </li>
             <li>
                 <a href="#section4" data-number="4">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label arrow_box">Home front protection</span>
+                    <span class="cd-dot active-dot"></span>
+                    <span class="cd-label arrow_box active">Home front protection</span>
                 </a>
             </li>
             <li>
                 <a href="#section5" data-number="5">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label arrow_box">Section 5</span>
+                    <span class="cd-dot active-dot"></span>
+                    <span class="cd-label arrow_box active">Broadly - trenches</span>
                 </a>
             </li>
             <li>
                 <a href="#section6" data-number="6">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label arrow_box">Section 6</span>
+                    <span class="cd-dot active-dot"></span>
+                    <span class="cd-label arrow_box active">Section 6</span>
                 </a>
             </li>
         </ul>
@@ -264,4 +264,43 @@ get_header(); ?>
         <aside class="image-bg-fixed-height-5"></aside>
         <p class="wp-cation-text">Caption: IMAGE%203</p>
     </section>
+<!--<script>
+    $(document).ready(function () {
+        $(document).on("scroll", onScroll);
+
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+
+            $('a').each(function () {
+                $(this).removeClass('active');
+            })
+            $(this).addClass('active');
+
+            var target = this.hash;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top+2
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+    });
+
+    function onScroll(event){
+        var scrollPosition = $(document).scrollTop();
+        $('nav a').each(function () {
+            var currentLink = $(this);
+            var refElement = $(currentLink.attr("href"));
+            if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+                $('nav ul li a').removeClass("active");
+                currentLink.addClass("active");
+            }
+            else{
+                currentLink.removeClass("active");
+            }
+        });
+    }
+</script>-->
 <?php get_footer(); ?>
