cc39583bbdffafa2f6debc4a99df139837acb6ed
Punal Chotrani <punalsc@gmail.com>
2016-04-12 17:12:43 +0100

    removed setTimeout

--- js/long-form.js
+++ js/long-form.js
@@ -72,11 +72,7 @@ $(document).ready(function(){
     });
 
     $("img.lazy").lazyload({
-        event : "sporty"
-    });
-
-    $(window).bind("load", function() {
-        var timeout = setTimeout(function() { $("img.lazy").trigger("sporty") }, 5000);
+        effect : "fadeIn"
     });
     /*END Lazy loading script*/
 
