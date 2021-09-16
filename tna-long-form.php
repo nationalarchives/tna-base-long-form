d2576822f3837d0b737f376c06a8b1f18ddf6a82
AshTNA <8880610+AshTNA@users.noreply.github.com>
2021-09-16 11:52:32 +0100

    Remove social media embeds

--- tna-long-form.php
+++ tna-long-form.php
@@ -55,36 +55,6 @@ get_header();
                     <figure>
                         <div class="lf-image-bg-fixed-height" style="background-image: url('<?php echo make_path_relative($image[0]); ?>')">
                             <?php get_template_part('breadcrumb'); ?>
-                            <div class="bt-archive-social-media">
-
-                                <div id="fb-root"></div>
-                                <script>(function (d, s, id) {
-                                        var js, fjs = d.getElementsByTagName(s)[0];
-                                        if (d.getElementById(id)) return;
-                                        js = d.createElement(s);
-                                        js.id = id;
-                                        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
-                                        fjs.parentNode.insertBefore(js, fjs);
-                                    }(document, 'script', 'facebook-jssdk'));</script>
-                                <div class="fb-share-button" data-href="http://www.nationalarchives.gov.uk/first-world-war/telecommunications-in-war/"
-                                     data-layout="button_count" tabindex="0"></div>
-
-                            <span tabindex="0">
-                                <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.nationalarchives.gov.uk/first-world-war/telecommunications-in-war/"
-                                   data-via="UKNatArchives">Tweet</a>
-                           <script>!function (d, s, id) {
-                                   var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
-                                   if (!d.getElementById(id)) {
-                                       js = d.createElement(s);
-                                       js.id = id;
-                                       js.src = p + '://platform.twitter.com/widgets.js';
-                                       fjs.parentNode.insertBefore(js, fjs);
-                                   }
-                               }(document, 'script', 'twitter-wjs');
-                           </script>
-                        </span>
-                            </div>
-
                             <div class="container-lf">
                                 <div class="intro-text">
                                     <h1 class="intro-heading"><?php the_title();  ?></h1>
