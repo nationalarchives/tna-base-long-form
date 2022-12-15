7e8a59435802b90d17b84854632ac59148e4ca16
Karl Kern <karl.kern@nationalarchives.gov.uk>
2022-12-15 09:16:37 +0000

    clean up code

--- .devops-deployment-scripts/permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm "/var/www/html/wp-content/themes/prep-env.sh"
-sudo chown -R apache:apache "/var/www/html/wp-content/themes/$1"
--- .devops-deployment-scripts/prep-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# make sure the directory exists
-mkdir -p "/var/www/html/wp-content/themes/$1"
